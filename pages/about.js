b73a3821d8d2c450b6780a6727f0064c123e95a5
shaunakg <gadkari.shaunak@gmail.com>
2020-08-19 17:20:18 +1000

    fixed about metadata

--- pages/about.js
+++ pages/about.js
@@ -11,19 +11,19 @@ const About = (props) => {
 
         <Head>
 
-            <title>{`About Me | Shaunak G - Web Developer`}</title>
-            <meta name="title" content={`About Me | Shaunak G - Web Developer`} />
+            <title>{`About | Shaunak G - Web Developer`}</title>
+            <meta name="title" content={`About | Shaunak G - Web Developer`} />
             <meta name="description" content={"Learn about who I am, what I do, and how my skills can be of use to your business."} />
 
             <meta property="og:type" content="website" />
             <meta property="og:url" content={"https://srg.codes/about"} />
-            <meta property="og:title" content={`Projects | Shaunak G - Web Developer`} />
+            <meta property="og:title" content={`About | Shaunak G - Web Developer`} />
             <meta property="og:description" content={"Learn about who I am, what I do, and how my skills can be of use to your business."} />
             <meta property="og:image" content={"https://og.srg.codes/**About**%20Me.png?theme=light&md=1&square=0&fontSize=100px&border=20px+solid+%23FFF&images=https%3A%2F%2Fcdn.srg.codes%2Fimages%2Ficon%2Ficon.svg&bgimage=triangle"} />
 
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content={"https://srg.codes/about"} />
-            <meta property="twitter:title" content={`About Me | Shaunak G - Web Developer`} />
+            <meta property="twitter:title" content={`About | Shaunak G - Web Developer`} />
             <meta property="twitter:description" content={"Learn about who I am, what I do, and how my skills can be of use to your business."} />
             <meta property="twitter:image" content={"https://og.srg.codes/**About**%20Me.png?theme=light&md=1&square=0&fontSize=100px&border=20px+solid+%23FFF&images=https%3A%2F%2Fcdn.srg.codes%2Fimages%2Ficon%2Ficon.svg&bgimage=triangle"} />
 
